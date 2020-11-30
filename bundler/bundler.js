1a854a69116c193beba245813549fae0a30f188a
Grischa Erbe <grischa.erbe@googlemail.com>
2020-12-01 00:59:56 +0100

    using parcel v2

--- bundler/bundler.js
+++ bundler/bundler.js
@@ -1,51 +1,47 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
-/* eslint-disable no-use-before-define */
-
-const Bundler = require('parcel-bundler')
+const Parcel = require('@parcel/core').default
 const path = require('path')
 const fs = require('fs')
 const AdmZip = require('adm-zip')
-
-const onError = (err) => {
-  console.log('There was an error bundling the poster.')
-  console.error(err)
-}
-
-const onBundled = () => {
-  const zip = new AdmZip()
-  const targetPath = path.join(__dirname, '..', 'IPDD Poster.zip')
-  const sourcePath = path.join(__dirname, '..', 'dist')
-  const fallbackPath = path.join(__dirname, '..', 'src', 'fallback')
-  if (fs.existsSync(targetPath)) fs.unlinkSync(targetPath)
-  zip.addLocalFolder(sourcePath)
-  zip.addLocalFolder(fallbackPath, 'fallback')
-  zip.writeZip(targetPath)
-}
+const rimraf = require('rimraf')
 
 const init = async () => {
+  // clean dist
+  const distDir = path.join(__dirname, '..', 'dist')
+  rimraf.sync(distDir)
+
+  // set entry point
   const entryPoint = path.join(__dirname, '..', 'src', 'index.html')
 
-  const options = {
-    outDir: './dist',
-    outFile: 'index.html',
-    publicUrl: '.',
-    watch: false,
-    cache: false,
-    minify: true,
-    scopeHoist: false,
-    target: 'browser',
-    logLevel: 3,
-    hmr: true,
-    hmrPort: 0,
-    sourceMaps: true,
-    detailedReport: false,
-    autoInstall: true,
-  }
+  // initialize bundler
+  try {
+    const bundler = new Parcel({
+      entries: entryPoint,
+      defaultConfig: require.resolve('@parcel/config-default'),
+      disableCache: true,
+      minify: true,
+      sourceMaps: false,
+      mode: 'production',
+      detailedReport: true,
+    })
 
-  const bundler = new Bundler(entryPoint, options)
-  bundler.on('bundled', onBundled)
-  bundler.on('buildError', onError)
-  await bundler.bundle()
+    // run bundler
+    await bundler.run()
+
+    // make zip
+    const zip = new AdmZip()
+    const targetPath = path.join(__dirname, '..', 'IPDD Poster.zip')
+    const sourcePath = path.join(__dirname, '..', 'dist')
+    const fallbackPath = path.join(__dirname, '..', 'src', 'fallback')
+    if (fs.existsSync(targetPath)) fs.unlinkSync(targetPath)
+    zip.addLocalFolder(sourcePath)
+    zip.addLocalFolder(fallbackPath, 'fallback')
+    zip.writeZip(targetPath)
+  } catch (err) {
+    console.log('There was an error bundling the poster.')
+    console.error(err)
+  }
 }
 
 init()
